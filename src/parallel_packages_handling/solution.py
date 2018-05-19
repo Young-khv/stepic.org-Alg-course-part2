9eef9b3cea235da558bfed2c2d99e6601e724eb4
Arkadiy Parinov <arkadiy.parinov@yandex.ru>
2018-05-19 20:48:43 +0300

    added process index to comparation of processes

--- src/parallel_packages_handling/solution.py
+++ src/parallel_packages_handling/solution.py
@@ -2,13 +2,14 @@ from queue import PriorityQueue
 
 
 class Process:
-    def __init__(self, start, duration):
+    def __init__(self, start, duration, pr_idx):
         self.start = start
         self.duration = duration
         self.end = start + duration
+        self.pr_idx = pr_idx
 
     def __gt__(self, other):
-        return self.start > other.start
+        return self.start > other.start and self.pr_idx > other.pr_idx
 
 
 def calculate_packages_handling_start(n, items_line):
@@ -16,14 +17,14 @@ def calculate_packages_handling_start(n, items_line):
     result = []
     items = list(map(lambda x: int(x), items_line.split()))
     for pr_idx, duration in enumerate(items[:n]):
-        pr = Process(0, duration)
+        pr = Process(0, duration, pr_idx)
         queue.put((pr, pr_idx))
     for duration in items[n:]:
         item = queue.get()
         pr_idx = item[1]
         prev_pr = item[0]
         result.append((pr_idx, prev_pr.start))
-        pr = Process(prev_pr.end, duration)
+        pr = Process(prev_pr.end, duration, pr_idx)
         queue.put((pr, pr_idx))
     while not queue.empty():
         item = queue.get()
